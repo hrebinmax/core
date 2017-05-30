9d89ecb80686c671550216e04ec9f183fa146e75
hrebinmax <yourluck@protonmail.com>
2017-05-30 21:42:28 +0300

    1.0

--- src/First_class.java
+++ src/First_class.java
@@ -1,8 +1,9 @@
 /**
+Max
  *
  */
 public class First_class {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Hi!!!");
     }
 }
